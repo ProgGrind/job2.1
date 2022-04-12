f2e9d013cc709fbdbfb4eb1204195753d0c983ff
ProgGrind <progressivegrind@gmail.com>
2022-04-12 21:44:37 +0300

    Added result in rub and kop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,17 @@
 package ru.netology
 
+import kotlin.math.roundToInt
+
 fun main() {
     print("Please type your payment amount: ")
-    val amount = readLine()?. toInt() ?: return
-    val commission = amount * 0.0075f
-    val result = if (commission > 35)
-        commission
-    else 35
+    val money = readLine()?. toInt() ?: return
+    val amount = money * 100
+    val commission = amount * 0.0075
+    val kop = (commission % 100).roundToInt()
+    val rubles = (commission / 100).roundToInt()
+    val result = if (commission > 3500)
+        "$rubles рублей $kop копеек"
+    else "35 рублей 0 копеек"
     println("Your commission: $result")
 
 }
